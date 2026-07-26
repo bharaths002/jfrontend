ce2f814f44ee1fbb15aeeec369f488cb3b774308
bharaths002 <mailmehere1820@gmail.com>
2026-07-26 23:05:05 +0530

    fix: use flat eslint config, downgrade errors to warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,13 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
     },
   },
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
 ];
 
 export default eslintConfig;
